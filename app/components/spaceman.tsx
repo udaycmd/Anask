38dc3b142a371a2ae7403f54b4484c72a96527f5
Uday Tiwari <udaytiwari360@gmail.com>
2025-12-12 22:39:17 +0530

    Fix: large font on mobile screens

--- app/components/spaceman.tsx
+++ app/components/spaceman.tsx
@@ -4,11 +4,11 @@ export default function Spaceman() {
     return (
         <div className="grid grid-cols-5 grid-rows-6 gap-2 border border-emerald-700 p-7 mx-auto rounded-xs container mb-15 text-gray-900 bg-purple-100">
             <div className="col-span-3 row-span-6 p-2">
-                <span className="bg-purple-500 rounded-xs p-1">Spaces</span>
+                <span className="bg-purple-500 rounded-xs p-1 text-xs">Spaces</span>
                 <div></div>
             </div>
             <div className="col-span-2 row-span-6 col-start-4">
-                <span className="bg-purple-500 rounded-xs p-1">
+                <span className="bg-purple-500 rounded-xs p-1 text-xs">
                     Trending discussions
                 </span>
             </div>
